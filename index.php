df51a89065d76356812ef217e093b5c213073ab9
midiam1 <admhimmeros@gmail.com>
2021-12-24 11:22:39 -0400

    +

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
+<style type="text/css">
+span {
+	font-size: 36px;
+}
+</style>
  <table width="100%" border="0" cellpadding="4" cellspacing="4">
   <tr>
     <td width="50%" rowspan="2" align="center" valign="middle"><img src="img/Black Folder Terminal-256.png" width="256" height="256" /></td>
-    <td>#webmin</td>
+    <td><h1>#Carpetas</h1></td>
   </tr>
   <tr>
-    <td>Instalación de <strong><a href="https://www.webmin.com/" title="Ir a ..." target="_blank">webmin</a></strong> en <strong>Ubuntu 18</strong>.</td>
+    <td>Crea varias carpetas + icono + descripción.</td>
   </tr>
 </table>
 <p><center></center></p>
-<hr/><p><strong>Webmin</strong> es una herramienta de configuración de sistemas accesible vía web para sistemas Unix, como GNU/Linux y OpenSolaris. Con él se pueden configurar aspectos internos de muchos sistemas libres, como el servidor web Apache, PHP, MySQL, DNS, Samba, DHCP, entre otros.</p>
+<hr/><p>&nbsp;</p>
